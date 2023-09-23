d22e566700165bc16656d19e83e3a03e3d6037cf
João Magalhães (Jaum) <dragon530698@gmail.com>
2023-09-23 17:49:18 -0300

    Lootlocker send score to leaderboard

--- Assets/ScoreSpaceJam/Scripts/Managers/LootLockerManager.cs
+++ Assets/ScoreSpaceJam/Scripts/Managers/LootLockerManager.cs
@@ -1,8 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LootLocker.Requests;
+using ScoreSpaceJam.Scripts.Utils;
+using System.Threading.Tasks;
 
-public class LootLockerManager : MonoBehaviour
+namespace ScoreSpaceJam.Scripts.Managers
 {
+    public class LootLockerManager : MonoBehaviour
+    {
+        public bool SessionStarted { get; private set; } = false;
 
+        private string playerIdentifier = null;
+        private const string defaultLeaderboardKey = "dev_leaderboard";
+
+        // REVIEW: eu odeio tudo sobre isso
+        public async Task<bool> TryStartGuestSession()
+        {
+            LootLockerGuestSessionResponse response = await StartGuestSessionAsync();
+            SessionStarted = response.success;
+
+            if (!response.success)
+            {
+                Debug.LogWarning($"[{this.name}]: ".Bold() + $"Failed to start guest session!");
+                return SessionStarted;
+            }
+
+            Debug.Log($"[{this.name}]: ".Bold() + $"Guest session successfully started!");
+            playerIdentifier = response.player_identifier;
+            return SessionStarted;
+        }
+
+        private Task<LootLockerGuestSessionResponse> StartGuestSessionAsync()
+        {
+            TaskCompletionSource<LootLockerGuestSessionResponse> tcs = new();
+            LootLockerSDKManager.StartGuestSession((response) =>
+            {
+                tcs.SetResult(response);
+            });
+
+            return tcs.Task;
+        }
+
+        public async Task<bool> SetPlayerName(string playerName)
+        {
+            if (!SessionStarted)
+            {
+                Debug.LogWarning($"[{this.name}]: ".Bold() + $"Tried to set player name without valid session!");
+                return false;
+            }
+
+            PlayerNameResponse response = await SetPlayerNameAsync(playerName);
+
+            if (!response.success)
+            {
+                Debug.LogWarning($"[{this.name}]: ".Bold() + $"Failed to set player name!");
+                return false;
+            }
+
+            Debug.Log($"[{this.name}]: ".Bold() + $"Player name set to {playerName}!");
+            return true;
+        }
+
+        private Task<PlayerNameResponse> SetPlayerNameAsync(string playerName)
+        {
+            TaskCompletionSource<PlayerNameResponse> tcs = new();
+            LootLockerSDKManager.SetPlayerName(playerName, (response) =>
+            {
+                tcs.SetResult(response);
+            });
+
+            return tcs.Task;
+        }
+
+        public async Task<bool> SubmitToLeaderboard(int score, string leaderboardKey = defaultLeaderboardKey)
+        {
+            if (!SessionStarted)
+            {
+                Debug.LogWarning($"[{this.name}]: ".Bold() + $"Tried to send score without valid session!");
+                return false;
+            }
+
+            LootLockerSubmitScoreResponse response = await SubmitScoreAsync(score, leaderboardKey);
+
+            if (!response.success)
+            {
+                Debug.LogWarning($"[{this.name}]: ".Bold() + $"Failed to send score to leaderboard with key {leaderboardKey}!");
+                return false;
+            }
+
+            Debug.Log($"[{this.name}]: ".Bold() + $"Score sent to leaderboard with key {leaderboardKey}!");
+            return true;
+        }
+
+        private Task<LootLockerSubmitScoreResponse> SubmitScoreAsync(int score, string leaderboardKey)
+        {
+            TaskCompletionSource<LootLockerSubmitScoreResponse> tcs = new();
+            LootLockerSDKManager.SubmitScore(playerIdentifier, score, leaderboardKey, (response) =>
+            {
+                tcs.SetResult(response);
+            });
+
+            return tcs.Task;
+        }
+    }
 }
---
+++ Assets/ScoreSpaceJam/Scripts/UI/LootLockerDemoScene/LootLockerDemoUI.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScoreSpaceJam.Scripts.Utils;
+using ScoreSpaceJam.Scripts.Managers;
+using TMPro;
+using UnityEngine.UI;
+
+namespace ScoreSpaceJam.Scripts.UI
+{
+    public class LootLockerDemoUI : MonoBehaviour
+    {
+        [SerializeField] private LootLockerManager lootlockerManager;
+
+        [Header("Login")]
+        [SerializeField] private GameObject loginGameObject;
+        [SerializeField] private TMP_InputField nameInputField;
+        [SerializeField] private Button confirmLoginButton;
+
+        [Header("Submit Score")]
+        [SerializeField] private GameObject submitScoreGameObject;
+        [SerializeField] private TMP_InputField scoreInputField;
+        [SerializeField] private Button submitScoreButton;
+
+        public async void OnPlayerLogin()
+        {
+            confirmLoginButton.interactable = false;
+            await lootlockerManager.TryStartGuestSession();
+            if (!lootlockerManager.SessionStarted)
+            {
+                Debug.LogWarning($"[{this.name}]: ".Bold() + $"Failed to start guest session! Cancelling interface flow.");
+                confirmLoginButton.interactable = true;
+                return;
+            }
+
+            // since lootlocker remembers the last set player name, only change the name if it is not null or empty
+            if (!string.IsNullOrEmpty(nameInputField.text))
+            {
+                bool success = await lootlockerManager.SetPlayerName(nameInputField.text);
+                if (!success)
+                {
+                    Debug.LogWarning($"[{this.name}]: ".Bold() + $"Failed to set player name! Cancelling interface flow.");
+                    confirmLoginButton.interactable = true;
+                }
+            }
+
+            submitScoreGameObject.SetActive(true);
+            loginGameObject.SetActive(false);
+        }
+
+        public async void OnSubmitPlayerScore()
+        {
+            submitScoreButton.interactable = false;
+            if (!lootlockerManager.SessionStarted)
+            {
+                Debug.LogWarning($"[{this.name}]: ".Bold() + $"Tried to send score without a valid session! Cancelling interface flow.");
+                submitScoreButton.interactable = true;
+                return;
+            }
+
+            if (!int.TryParse(scoreInputField.text, out int castedScore))
+            {
+                Debug.LogWarning($"[{this.name}]: ".Bold() + $"Failed to parse score from input field! Cancelling interface flow.");
+                submitScoreButton.interactable = true;
+                return;
+            }
+
+            bool success = await lootlockerManager.SubmitToLeaderboard(castedScore);
+            if (!success)
+            {
+                Debug.LogWarning($"[{this.name}]: ".Bold() + $"Failed to send score! Cancelling interface flow.");
+            }
+
+            submitScoreButton.interactable = true;
+        }
+    }
+}
